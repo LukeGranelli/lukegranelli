273e9dbd90611bb5f4586bdc4a6fdad008d2d281
ST10482816 <ST10482816@vcconnect.edu.za>
2025-11-21 13:49:19 +0200

    style: header colour and design adjustments. Feat: added localstorage to keep track of theme choice for dark/light modes

--- scripts.js
+++ scripts.js
@@ -18,8 +18,15 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-
-
+    const themeToggle = document.getElementById("theme-toggle");
+    themeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+        localStorage.setItem("theme", document.body.classList.contains("light-mode") ? "light" : "dark");
+    });
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "light") {
+        document.body.classList.add("light-mode");
+    }
 
 
 
